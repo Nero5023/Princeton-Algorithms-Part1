938c002dbc4adc2403b124d18f1ec3173b319a9e
Nero Zuo <502350123@qq.com>
2017-03-19 23:44:41 +0800

    Union find implementation in python

---
+++ Percolation/WeightedQuickUnionUF.py
@@ -0,0 +1,52 @@
+class WeightQuickUnionUF():
+    """docstring for WeightQuickUnionUF"""
+    def __init__(self, n):
+        self.count  = n
+        self.parent = [i for i in range(0, n)]
+        self.size   = [1 for _ in range(0, n)]
+        self.maxCom = [i for i in range(0, n)] 
+
+    def validate(p):
+        n = len(self.parent)
+        if p < 0 or p > n:
+            raise IndexError("Index out of bound", p)
+
+    def find(p):
+        self.validate()
+        while p != self.parent[p]:
+            p = parent[p]
+        return p
+
+    def findMax(p):
+        self.validate()
+        while p != self.maxCom[p]:
+            p = self.maxCom[p]
+        return p
+
+    def connected(p, q):
+        return self.find(p) == self.find(q)
+
+    def union(p, q):
+        rootP = find(p);
+        rootQ = find(q);
+        if rootQ == rootP:
+            return
+
+        if self.size(rootP) < self.size(rootQ):
+            self.parent[rootP] = rootQ
+            self.size[rootQ] += self.size[rootP]
+        else:
+            self.parent[rootQ] = rootP
+            self.size[rootP] += self.size[rootQ]
+
+        maxP = findMax(p);
+        maxQ = findMax(q);
+        if maxP > maxQ:
+            self.maxCom[maxQ] = maxP
+        else:
+            self.maxCom[maxP] = maxQ
+
+        self.count -= 1
+
+
+if __name__ == '__main__':
